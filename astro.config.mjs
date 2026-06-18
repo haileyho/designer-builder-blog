4afd9c4cdc5dcd4de2153f07d7e23a02f6df8e31
haileyho <87190790+haileyho@users.noreply.github.com>
2026-06-18 09:34:02 -0400

    fix: remove @astrojs/sitemap to fix build crash

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://haileyho.github.io/designer-builder-blog',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx()],
 });
